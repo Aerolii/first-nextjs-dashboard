feb7b7734b0261998ed5e22ceba8246aca24dac6
weenie89 <weenie89@Zhang.local>
2024-04-01 16:36:52 +0800

    feat: 使用 Form action 在服务端提交表单；使用 zod 验证表单数据；提交表单后重新验证访问url、获取数据并将用户页面重定向

---
+++ app/lib/actions.ts
@@ -0,0 +1,46 @@
+'use server';
+
+import { sql } from '@vercel/postgres';
+import { z } from 'zod';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export default async function createInvoice(formData: FormData) {
+  // const rawFormData = {
+  //   customerId: formData.get('customerId'),
+  //   amount: formData.get('amount'),
+  //   status: formData.get('status'),
+  // };
+  // const rawFormData = Object.fromEntries(formData.entries());
+  // console.log(rawFormData);
+
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  const amountInCent = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+  await sql`
+  INSERT INTO invoices (customer_id, amount, status, date)
+  VALUES (${customerId}, ${amountInCent}, ${status}, ${date})
+  `;
+
+  const pathname = '/dashboard/invoices';
+
+  // 数据库更新后，/dashboard/invoices路径将重新验证，并且将从服务器获取新数据。
+  revalidatePath(pathname);
+
+  // 将用户重定向回该/dashboard/invoices页面
+  redirect(pathname);
+}
